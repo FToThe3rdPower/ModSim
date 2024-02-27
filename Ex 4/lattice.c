00f2ebe1f67f13037babb11734d649aa6b0a021b
Trey Grijalva <FToThe3rdPower@users.noreply.github.com>
2024-02-27 10:05:07 +0100

    complete FCC cell

--- Ex 4/lattice.c
+++ Ex 4/lattice.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 // Include the standard library for general purpose functions like memory allocation, process control, conversions, etc.
 #include <stdlib.h>
+// import standard bool for storing only the unique data before writin it to the file
+#include <stdbool.h>
+
+//check for duplicates to avoid russian nesting doll particles that violate the Pauli exc. prin.
+//They will appear to multiply with the movement code
+/*
+bool is_duplicate(Coordinate* arr, size_t size, Coordinate coord)
+{
+  for (size_t i = 0; i < size; ++i)
+  {
+    if (arr[i].x == coord.x && arr[i].y == coord.y && arr[i].z == coord.z)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+*/
+
 
 // Entry point of the program with command line arguments
 // argc: argument count, argv: argument vector (list of arguments)
@@ -12,7 +31,8 @@ int main(int argc, char *argv[]){
     int N = atoi(argv[1]);
 
     //determining the total number of particle coords. 1 whole particle per fcc face, 6 faces, N^3 for the dimensions
-    int totalParticles = 11*N*N*N;//N*N*N;
+    int totalParticles = 14*N*N*N;//N*N*N;
+
 
     // Set the spacing between points in the lattice
     float spacing = 2.0;
@@ -29,22 +49,34 @@ int main(int argc, char *argv[]){
     float y[totalParticles];
     float z[totalParticles]; // These arrays can store N^3 points as it's a 3D cubic lattice
 
-    // Initialize a counter for indexing the arrays, counts the corners
+    //An empty array to store only unique coordinates
+ /*   size_t max_unique_coords = totalParticles;
+    Coordinate unique_coords[max_unique_coords];
+    size_t unique_count = 0;
+*/
+
+    // Initialize a counter for indexing the arrays
     int m = 0;
 
     // Declare a file pointer for writing the coordinates to a file
-    FILE *dataFilePntr;
     // Open the file "simple_cubic.xyz" for writing.
     //w modes means if the file doesn't exist, it will be created.
-    dataFilePntr = fopen("fcc.dat", "w");
+    //an err will be caught by the if
+    FILE *dataFilePntr = fopen("fcc.dat", "w");
+    if (dataFilePntr == NULL)
+    {
+    printf("Error opening file!\n");
+    return 1;
+    }
 
+    //The first 4 lines are info for the WebGL visualizer to know # of 'particles' and box size
     // Write the total number of points to the file as the first line
-    fprintf(dataFilePntr, "totalParticles: %i, space: %f, hlfSpace: %f\n", totalParticles, spacing, halfSpacing);
+    fprintf(dataFilePntr, "%i\n", totalParticles);
     // Write the lattice dimensions to the file.
     //These lines are placeholders and may not represent actual physical dimensions.
-    /*fprintf(dataFilePntr, "%f %f\n", 0.0, N*spacing);
     fprintf(dataFilePntr, "%f %f\n", 0.0, N*spacing);
-    fprintf(dataFilePntr, "%f %f\n", 0.0, N*spacing);*/
+    fprintf(dataFilePntr, "%f %f\n", 0.0, N*spacing);
+    fprintf(dataFilePntr, "%f %f\n", 0.0, N*spacing);
 
 
     // Generate the coordinates for each point in the cell
@@ -72,17 +104,18 @@ int main(int argc, char *argv[]){
                 y[m+3] = (float)j * spacing + halfSpacing;
                 z[m+3] = (float)k * spacing + spacing;
 
-                //top
+                //top, 5th
                 x[m+4] = (float)i * spacing + halfSpacing;
                 y[m+4] = (float)j * spacing + spacing;
                 z[m+4] = (float)k * spacing + halfSpacing;
 
-                //bottom
+                //bottom, 6th
                 x[m+5] = (float)i * spacing + halfSpacing;
                 y[m+5] = (float)j * spacing;
                 z[m+5] = (float)k * spacing + halfSpacing;
+                
 
-                //corner 1  !!! breaks the visualizer
+                //corner 1 
                 x[m+6] = (float)i * spacing;
                 y[m+6] = (float)j * spacing;
                 z[m+6] = (float)k * spacing;
@@ -98,29 +131,29 @@ int main(int argc, char *argv[]){
                 z[m+8] = (float)k * spacing + spacing;
 
                 //corner 4
-                x[m+7] = (float)i * spacing + spacing;
-                y[m+7] = (float)j * spacing;
-                z[m+7] = (float)k * spacing + spacing;
-
-                //corner 5
-                x[m+7] = (float)i * spacing + spacing;
-                y[m+7] = (float)j * spacing + spacing;
-                z[m+7] = (float)k * spacing;
-
-                //corner 6
-                x[m+8] = (float)i * spacing;
-                y[m+8] = (float)j * spacing;
-                z[m+8] = (float)k * spacing + spacing;
-
-                //corner 7
                 x[m+9] = (float)i * spacing + spacing;
                 y[m+9] = (float)j * spacing;
-                z[m+9] = (float)k * spacing;
+                z[m+9] = (float)k * spacing + spacing;
 
-                //corner 8
-                x[m+10] = (float)i * spacing;
+                //corner 5
+                x[m+10] = (float)i * spacing + spacing;
                 y[m+10] = (float)j * spacing + spacing;
                 z[m+10] = (float)k * spacing;
+
+                //corner 6
+                x[m+11] = (float)i * spacing;
+                y[m+11] = (float)j * spacing;
+                z[m+11] = (float)k * spacing + spacing;
+
+                //corner 7
+                x[m+12] = (float)i * spacing + spacing;
+                y[m+12] = (float)j * spacing;
+                z[m+12] = (float)k * spacing;
+
+                //corner 8
+                x[m+13] = (float)i * spacing;
+                y[m+13] = (float)j * spacing + spacing;
+                z[m+13] = (float)k * spacing;
                 
                 
                 // Write the coordinates to the file
@@ -135,6 +168,11 @@ int main(int argc, char *argv[]){
                 fprintf(dataFilePntr, "%f %f %f\n", x[m+8], y[m+8], z[m+8]);
                 fprintf(dataFilePntr, "%f %f %f\n", x[m+9], y[m+9], z[m+9]);
                 fprintf(dataFilePntr, "%f %f %f\n", x[m+10], y[m+10], z[m+10]);
+                fprintf(dataFilePntr, "%f %f %f\n", x[m+11], y[m+11], z[m+11]);
+                fprintf(dataFilePntr, "%f %f %f\n", x[m+12], y[m+12], z[m+12]);
+                fprintf(dataFilePntr, "%f %f %f\n", x[m+13], y[m+13], z[m+13]);
+
+
 
                 // Print the coordinates to the console for verification
                 printf("\n%f %f %f\n", x[m], y[m], z[m]);
@@ -148,13 +186,20 @@ int main(int argc, char *argv[]){
                 printf("%f %f %f\n", x[m+8], y[m+8], z[m+8]);
                 printf("%f %f %f\n", x[m+9], y[m+9], z[m+9]);
                 printf("%f %f %f\n", x[m+10], y[m+10], z[m+10]);
+                printf("%f %f %f\n", x[m+11], y[m+11], z[m+11]);
+                printf("%f %f %f\n", x[m+12], y[m+12], z[m+12]);
+                printf("%f %f %f\n", x[m+13], y[m+13], z[m+13]);
+
 
                 // Increment the counter for the next cell
-                m+=11;
+                m+=14;
                 
             }
         }
     }
+
+    //output some relevant info
+    printf("total coords: %i, space: %f, hlfSpace: %f\n", totalParticles, spacing, halfSpacing);
 
     // Close the file after writing all the coordinates
     fclose(dataFilePntr);
