dd116613e05af0f4336deb94ae840373fad8bdef
Trey Grijalva <FToThe3rdPower@users.noreply.github.com>
2024-03-13 21:22:45 +0100

    outputs the necessary data file for histogramming

--- ex6/workingNVT.c
+++ ex6/workingNVT.c
@@ -157,18 +157,16 @@ void set_packing_fraction(void){
 
 
 //distance calc func
-int distToNeighbors(int numParticles, int step)
+int distToNeighbors(int numParticles, FILE *distFile, int step)
 {
     //vars 'n arrs we'll need
     int e, n, dim;
     double pythag;
     double cartDistArr[numParticles][numParticles-1][NDIM];
 
-    //file we'll need
-    FILE *distFile = fopen("distances.txt", "w");
 
     //loop for the main character particle
-    for(n=0; n <= numParticles; n++)
+    for(n=0; n < numParticles; n++)
     {
         //loop for the  "extra" supporting role
         for(e=n+1; e < numParticles; e++)
@@ -176,12 +174,6 @@ int distToNeighbors(int numParticles, int step)
             //gotta consider each dimensions
             for(dim=0; dim < NDIM; dim++)
             {
-                //make the header text for the file
-                if(n==0 & e==1 & dim==0)
-                {
-                    fprintf(distFile, "Step\tp1\tp2\tDistance,\tdensity:%lf\n", density);
-                }
-
 
                 //write to the array for the distance
                 cartDistArr[n][e][dim] = r[n][dim] - r[e][dim];
@@ -199,28 +191,25 @@ int distToNeighbors(int numParticles, int step)
                             n, e, cartDistArr[n][e][dim]);
                         break;
 
-                    //final coord dist found, time to calc the distance
                     case 2:
                         printf("particle %i_z - particle %i_z = %lf\n",
                             n, e, cartDistArr[n][e][dim]);
-
-                        //pythagorize to get the total distance from particle to particle
-                        pythag = sqrt(pow(cartDistArr[n][e][dim],2 ) + pow(cartDistArr[n][e][dim-1],2) + pow(cartDistArr[n][e][dim-2],2 ));
-                        
-
-                        //put it in da file
-                        fprintf(distFile, "%i\t%i\t%i\t%lf\n", step, n, e, pythag);
-
-                        //print it to be sure
-                        printf("dist p%i to p%i = %lf\n\n", n, e, pythag);
                         break;
                 }
             }
+            //pythagorize to get the total distance from particle to particle
+            pythag = sqrt(pow(cartDistArr[n][e][dim],2 ) + pow(cartDistArr[n][e][dim-1],2) + pow(cartDistArr[n][e][dim-2],2));
+
+            //print it to be sure
+            printf("dist p%i to p%i = %lf\n\n", n, e, pythag);
+
+            //it puts the data in the file or it gets the hose again
+            fprintf(distFile, "%i\t%i\t%i\t%lf\n", step, n, e, pythag);
+            //this is located here because the loop over all 3 dimensions needs to happen first
+            //to get the pythag distance before we write to the file
         }
     }
 
-    //close the file now that we're done
-    fclose(distFile);
     if (sizeof(cartDistArr) > 0)
     {
         return 0;
@@ -233,7 +222,14 @@ int distToNeighbors(int numParticles, int step)
 
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
+    //file we'll need
+    FILE *distFile = fopen("distances.txt", "a");
+    //header text
+    fprintf(distFile, "Step\tp1\tp2\tDistance,\tdensity:%lf\n", density);
+
+
     //local vars
     double boxVol;
 
@@ -282,23 +278,24 @@ int main(int argc, char* argv[]){
     int accepted = 0;
     int step, n;
     //Perform MC moves
-    for(step = 0; step < mc_steps; ++step){
-        for(n = 0; n < n_particles; ++n){
+    for(step = 0; step < mc_steps; ++step)
+    {
+        for(n = 0; n < n_particles; ++n)
+        {
             accepted += move_particle();
         }
 
-        //print and write the move every output_step, default is 100
-        if(step % output_steps == 0 & step > 0){
-            printf("Step %d.\tMove acceptance: %f.\n", step, (double)accepted
-                / (n_particles * output_steps));
+        //print and write the move per every output_step, default is 100
+        if(step % output_steps == 0 & step > 0)
+        {
+            printf("\n\n\n\nStep %d.\tMove acceptance: %f.\n\n", step, (double)accepted / (n_particles * output_steps));
             accepted = 0;
             write_data(step);
-            distToNeighbors(n_particles, step);
+            distToNeighbors(n_particles, distFile, step);
         }
+
     }
-
-
-
-
+    //close the file now that we're done
+    fclose(distFile);
     return 0;
 }
